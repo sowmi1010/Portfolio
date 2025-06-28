8aec3ec0c37ee84d1d2334bcd51c07cc577f7272
sowmi1010 <tamilsowmi1010@gmail.com>
2025-06-28 17:23:21 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // we will toggle a class
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
